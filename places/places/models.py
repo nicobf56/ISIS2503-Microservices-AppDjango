06b2f7ee843024b6603c9f00797612936b83398c
nicobf56 <n.bedoyaf@uniandes.edu.co>
2024-05-19 20:42:42 -0500

    ....

---
+++ places/places/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Variable(models.Model):
+    name = models.CharField(max_length=50, null = False)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+
+
